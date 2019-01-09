cb8f3336d9df8dea6307152e4cf03fa5159e3df8
Thomas Frei <mail@thomasfrei.de>
2019-01-10 00:41:02 +0100

    add slate editor bare component

---
+++ app/components/SlateEditor/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * SlateEditor
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class SlateEditor extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+SlateEditor.propTypes = {};
+
+export default SlateEditor;
---
+++ app/components/SlateEditor/tests/index.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import { enzymeFind } from 'styled-components/test-utils';
+
+// import SlateEditor from '../index';
+
+describe('<SlateEditor />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
