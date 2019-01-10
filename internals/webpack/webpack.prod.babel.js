69eb79bd8e71fa2a6ab5116b2b86475e74ac7cde
Thomas Frei <mail@thomasfrei.de>
2019-01-10 01:25:40 +0100

    remove favicons, add 512 icon

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -121,9 +121,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'SynCode',
+      short_name: 'SynCode',
+      description: 'Encode multiple videos simultaneously',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
